5635f62889a6a704cbc3d8ced24b84209c824dce
b0443301 <g02361081@gmail.com>
2017-05-26 11:56:07 +0800

    初始版本
    完成 version 協議

---
+++ index.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+if(isset($_GET['command'])){
+	$command = $_GET['command'];
+}else{
+	$command = '';
+}
+
+if ($command === 'version'){
+	$connect = new PDO('mysql:host=localhost;dbname=order_system;charset=utf8', 'osadmin', '0983451956');
+	$statement = $connect->query('CALL GetVersion()');
+	foreach($statement as $row){
+		$jsonArray = array($command => $row[$command]);
+		echo json_encode($jsonArray);
+	}
+}
+
+?>

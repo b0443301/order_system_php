fc20552b3827125f5bc54fabf30b9617a2db3db8
b0443301 <g02361081@gmail.com>
2017-05-27 20:56:38 +0800

    第三版
    完成 register 協議

--- index.php
+++ index.php
@@ -59,6 +59,54 @@ if ($command === 'version'){
 		$jsonArray = array('result' => $result);
 		echo json_encode($jsonArray);
 	}
+}else if($command === 'register'){
+	if(isset($_GET['account'])){
+		$account = $_GET['account'];
+	}
+	if(isset($_GET['password'])){
+		$password = $_GET['password'];
+	}
+	if(isset($_GET['mail'])){
+		$mail = $_GET['mail'];
+	}
+	if(isset($_GET['username'])){
+		$username = $_GET['username'];
+	}
+	if(isset($_GET['telephone'])){
+		$telephone = $_GET['telephone'];
+	}
+	if(isset($_GET['address'])){
+		$address = $_GET['address'];
+	}
+	if((!isset($account)) or (!isset($password)) or (!isset($mail)) or (!isset($username)) or (!isset($telephone)) or (!isset($address))){
+		$result = 'register_fail';
+	}
+	
+	$connect = new PDO('mysql:host=localhost;dbname=order_system;charset=utf8', 'osadmin', '0983451956');
+	
+	if(!isset($result)){
+		$statement = $connect->query('SELECT id FROM register WHERE account = '.'\''.$account.'\''.' ORDER BY id DESC LIMIT 1');
+		foreach($statement as $row){
+			$result = 'register_same_account';
+		}
+	}
+	
+	$id = '0';
+	$random = '0';
+	
+	if(!isset($result)){
+		$statement = $connect->query('INSERT INTO register VALUES (\''.$id.'\',\''.$account.'\',\''.$password.'\',\''.$random.'\',\''.$mail.'\',\''.$username.'\',\''.$telephone.'\',\''.$address.'\')');
+		$result = 'register_success';
+		$session = session_id();
+	}
+	
+	if(isset($session)){
+		$jsonArray = array('result' => $result, 'session' => $session);
+		echo json_encode($jsonArray);
+	}else{
+		$jsonArray = array('result' => $result);
+		echo json_encode($jsonArray);
+	}
 }
 
 ?>

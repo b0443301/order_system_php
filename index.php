26bf69eb3c7097c2a9e64f98ddacaf6207bb754c
b0443301 <g02361081@gmail.com>
2017-05-27 14:28:22 +0800

    第二版
    完成 login 協議

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 ﻿<?php
 
+if(isset($_GET['session'])){
+	session_id($_GET['session']);
+}
+
+session_start();
+
 if(isset($_GET['command'])){
 	$command = $_GET['command'];
 }else{
@@ -8,9 +14,49 @@ if(isset($_GET['command'])){
 
 if ($command === 'version'){
 	$connect = new PDO('mysql:host=localhost;dbname=order_system;charset=utf8', 'osadmin', '0983451956');
-	$statement = $connect->query('CALL GetVersion()');
+	$statement = $connect->query('SELECT version FROM global ORDER BY id DESC LIMIT 1');
 	foreach($statement as $row){
 		$jsonArray = array($command => $row[$command]);
+		echo json_encode($jsonArray);
+	}
+}else if($command === 'login'){
+	if(isset($_GET['account'])){
+		$account = $_GET['account'];
+	}
+	if(isset($_GET['password'])){
+		$password = $_GET['password'];
+	}
+	if(!isset($account)){
+		$account = '';
+	}
+	if(!isset($password)){
+		$password = '';
+	}
+	
+	$connect = new PDO('mysql:host=localhost;dbname=order_system;charset=utf8', 'osadmin', '0983451956');
+	
+	$statement = $connect->query('SELECT id FROM register WHERE account = '.'\''.$account.'\''.' AND password = '.'\''.$password.'\''.' ORDER BY id DESC LIMIT 1');
+	foreach($statement as $row){
+		$result = 'login_success';
+		$session = session_id();
+	}
+	
+	if(!isset($result)){
+		$statement = $connect->query('SELECT id FROM register WHERE account = '.'\''.$account.'\''.' ORDER BY id DESC LIMIT 1');
+		foreach($statement as $row){
+			$result = 'login_fail';
+		}
+	}
+	
+	if(!isset($result)){
+		$result = 'login_need_register';
+	}
+	
+	if(isset($session)){
+		$jsonArray = array('result' => $result, 'session' => $session);
+		echo json_encode($jsonArray);
+	}else{
+		$jsonArray = array('result' => $result);
 		echo json_encode($jsonArray);
 	}
 }
